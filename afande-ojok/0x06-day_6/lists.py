2753bd8fb0c516f18c36378b601a47803bd70549
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-09-10 20:12:51 +0300

    Lists

---
+++ afande-ojok/0x06-day_6/lists.py
@@ -0,0 +1,9 @@
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+print(months)
+print(months[0])
+print(months[8])
+print(months[-1])
+print(months[:6])
+print(months[6:])
+print(months[8:-1])
