b78e420791ecede864db0a395c5e951d7bafeb04
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-09-11 23:01:34 +0300

    Working with string and list

--- afande-ojok/0x07-day_7/random_list.py
+++ afande-ojok/0x07-day_7/random_list.py
@@ -26,3 +26,40 @@ print("The length of the list variable 'months' is: {}".format(len(months)))
 
 greeting = "Welcome to Yearn AI"
 print("The length of the string variable 'greeting' is: {}".format(len(greeting)))
+print("==============================================================================================================")
+
+# 'in' or 'not in' - String
+
+greeting = "Welcome to Yearn AI"
+
+print('ai' in greeting)
+print('ai' not in greeting)
+print('AI' in greeting)
+print('AI' not in greeting)
+print("==============================================================================================================")
+
+
+# 'in' or 'not in' - List
+num = [1, 2, 3, 4, 6, 8, 12]
+
+print(5 in num)
+print(5 not in num)
+print("==============================================================================================================")
+
+# Mutable and Ordered
+# List is Mutable and Order
+# String is Immutable and Order
+
+my_list = [1, 3, 5, 7, 9, 11]
+print(my_list)
+my_list[2] = 4
+print(my_list)
+my_list[3:] = 4, 6, 8
+print(my_list)
+print("==============================================================================================================")
+
+my_string = 'Hello Afande Ojok'
+print(my_string[:5])
+print(my_string[5:])
+print(my_string[-1])
+print(my_string[5]) # Prints the space separating Hello and Afande
