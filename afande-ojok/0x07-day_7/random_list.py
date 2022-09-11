ea72f18ed4edf49bab042ccdae01df2c6bc0f5f5
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-09-11 23:24:16 +0300

    Working with List Data Structure

--- afande-ojok/0x07-day_7/random_list.py
+++ afande-ojok/0x07-day_7/random_list.py
@@ -63,3 +63,43 @@ print(my_string[:5])
 print(my_string[5:])
 print(my_string[-1])
 print(my_string[5]) # Prints the space separating Hello and Afande
+print("==============================================================================================================")
+
+# List Indexing
+month = 8
+days_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+# use list indexing to determine the number of days in month
+num_days = days_in_month[7]
+num_days_1 = days_in_month[month - 1]
+
+print(num_days)
+print(num_days_1)
+print("==============================================================================================================")
+
+# Slicing Lists
+# Select the three most recent dates from this list using list slicing notation. Hint: negative indexes work in slices!
+eclipse_dates = ['June 21, 2001', 'December 4, 2002', 'November 23, 2003',
+                 'March 29, 2006', 'August 1, 2008', 'July 22, 2009',
+                 'July 11, 2010', 'November 13, 2012', 'March 20, 2015',
+                 'March 9, 2016']
+                 
+                 
+# TODO: Modify this line so it prints the last three elements of the list
+print(eclipse_dates[-3:])
+
+# List and String
+sentence1 = "I wish to register a complaint."
+sentence2 = ["I", "wish", "to", "register", "a", "complaint", "."]
+
+sentence2[6] = '!'
+print(sentence2)
+
+sentence2[0] = "Our Majesty"
+print(sentence2)
+
+# sentence1[30] = '!'
+# print(sentence1) - Returns an error: TypeError: 'str' object does not support item assignment
+
+sentence2[0:2] = ['We', 'want']
+print(sentence2)
