c3b5873d421e25091a3c2f49808705f27f986540
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-09-20 18:46:12 +0300

    Working with Dictionary

---
+++ afande-ojok/0x16-day_16/dictionary.py
@@ -0,0 +1,27 @@
+# Working with Dictionary
+
+phase1_route1 = {
+    'day1': 1678,
+    'day2': 1629,
+    'day3': 1593,
+    'day4': 1529,
+    'day5': 1486,
+    'day6': 1453,
+    'day7': 1381,
+    'day8': 1353,
+    'day9': 1321,
+    'day10': 1299,
+    'day11': 1312,
+    'day12': 1334,
+    'day13': 1301,
+    'day14': 1287,
+    'day15': 1276,
+    'day16': 1289,
+    'day17': 1511,
+    'day18': 1264,
+    'day19': 1250,
+    'day20': 1273
+}
+
+print(phase1_route1)
+print(phase1_route1.get('day19'))
