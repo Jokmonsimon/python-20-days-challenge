ad3a085d37e9ac6830c936a86a41234a3ccb990c
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-09-25 05:09:09 +0300

    Working with if, elif & else

--- afande-ojok/pc_20_days_set_2/0x01-day_1/if_statements.py
+++ afande-ojok/pc_20_days_set_2/0x01-day_1/if_statements.py
@@ -1,9 +1,24 @@
 # Working with Conditional Flow in Python
 
 n = 11
-n = 26
+# n = 26
 
 if n % 2 == 0:
     print("Number {} is even!".format(str(n)))
 else:
     print("Number {} is odd!".format(str(n)))
+
+# if, elif, else
+
+season = 'Winter'
+
+if season == 'Spring':
+    print('Pland the garden!')
+elif season == 'Summer':
+    print('Water the gardern!')
+elif season == 'Fall':
+    print('Harvest the garden!')
+elif season == 'Winter':
+    print('Stay indoors!')
+else:
+    print('Unrecognized season!')
