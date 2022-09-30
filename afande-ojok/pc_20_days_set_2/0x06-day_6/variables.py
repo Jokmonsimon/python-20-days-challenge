199c2aec171cfeee2aa778b3f39aebce26eb631d
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-09-30 05:26:10 +0300

    Working with variables

---
+++ afande-ojok/pc_20_days_set_2/0x06-day_6/variables.py
@@ -0,0 +1,9 @@
+# Working with Variables in Python
+
+phase = 'Phase 1'
+route = 'Route 5'
+day = 'Day 5'
+time = '11 minutes 08 seconds 95 milliseconds'
+speed = 3.40
+
+print("It's {}: {} on {}! My time was {} with an average speed of {} per kilometer".format(day, phase, route, time, speed))
