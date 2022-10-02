2d7e5362ac6a167a89811b67b6ba4a8340fd15eb
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-10-02 04:12:18 +0300

    Update points_prize file

--- afande-ojok/pc_20_days_set_2/0x07-day_7/points_prize.py
+++ afande-ojok/pc_20_days_set_2/0x07-day_7/points_prize.py
@@ -4,14 +4,16 @@ points = int(input("Plese enter your Points: "))
 prize = ["Wooden rabbit", "no prize", "Wafer-thin mint", "Penguin"]
 
 if points <= 0:
-    print("Points can only take on positive integer values up to 200.")
+    result = "Points can only take on positive integer values up to 200."
 elif points <= 50:
-    print("Congratulations! You have won {} for {} points earned!".format(prize[0], points))
+    result = "Congratulations! You have won {} for {} points earned!".format(prize[0], points)
 elif points <= 150:
-    print("Sorry! {} for {} points earned!. Please try again!".format(prize[1], points))
+    result = "Sorry! {} for {} points earned!. Please try again!".format(prize[1], points)
 elif points <= 180:
-    print("Congratulations! You have won {} for {} points earned!".format(prize[2], points))
+    result = "Congratulations! You have won {} for {} points earned!".format(prize[2], points)
 elif points <= 200:
-    print("Congratulations! You have won {} for {} points earned!".format(prize[3], points))
+    result = "Congratulations! You have won {} for {} points earned!".format(prize[3], points)
 else:
-    print("Sorry! Prizes available for positive integer values up to 200!")
+    result = "Sorry! Prizes available for positive integer values up to 200!"
+
+print(result)
