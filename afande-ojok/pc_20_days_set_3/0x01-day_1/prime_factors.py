c44f2a4d437e2123f27ca5e2cf31c90831488856
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-10-15 15:16:09 +0300

    New challenge

---
+++ afande-ojok/pc_20_days_set_3/0x01-day_1/prime_factors.py
@@ -0,0 +1,16 @@
+# Print Prime Factors
+
+def prime_factors(number):
+    # Start with two, which is the first prime
+    factor = 2
+    # Keep going until the factor is larger than the number
+    while factor <= number:
+        # Check if factor is a divisor of number
+        if number % factor == 0:
+            # If it is, print it and divide the original number
+            print(factor)
+            number = number / factor
+        else:
+            # If it's not, increment the factor by 1
+            factor += 1
+    return 'Done!'
