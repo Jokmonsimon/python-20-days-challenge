35ba321198ae4c0cda12f68bf4f54863f8b5579e
Afande Ojok <simonpeter.oj@gmail.com>
2022-10-19 17:56:29 +0300

    Working with function in Python

---
+++ afande-ojok/pc_20_days_set_3/0x05-day_5/print_function.py
@@ -0,0 +1,6 @@
+# Working with function in python
+x = 1
+def function(): # The fuction takes no parameter(s) or argument(s) 
+    x = [1, 5, 8, 2, 35, 12, 43, 6]
+    print(sorted(x))
+function() # No parameter(s) or argument(s) should be passed inside the function 
