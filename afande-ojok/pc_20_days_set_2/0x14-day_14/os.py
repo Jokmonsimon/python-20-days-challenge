b92a18a71dd737723b393a9e46749d0071f9c7a1
Afande Ojok <simonpeter.oj@gmail.com>
2022-10-20 14:33:10 +0300

    os path

--- afande-ojok/pc_20_days_set_2/0x14-day_14/os.py
+++ afande-ojok/pc_20_days_set_2/0x14-day_14/os.py
@@ -11,6 +11,13 @@ def main():
     print(os.name)
 
     # Check for item existence and type
+    print("Item exists: ", str(path.exists("textfile.txt")))
+    print("Item is a file: ", path.isfile("textfile.txt"))
+    print("Item is a directory: ", path.isdir("textfile.txt"))
+
+    # Work with file paths
+    print("Item's path: ", path.realpath("textfile.txt"))
+    print("Item's path and name: ", path.split(path.realpath("textfile.txt")))
 
 if __name__ == "__main__":
     main()
