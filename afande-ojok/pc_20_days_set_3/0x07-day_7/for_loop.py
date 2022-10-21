5505f398b39f79ae8d8e7b475eec339e86016241
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-10-21 22:03:18 +0300

    Working with for loop

--- afande-ojok/pc_20_days_set_3/0x07-day_7/for_loop.py
+++ afande-ojok/pc_20_days_set_3/0x07-day_7/for_loop.py
@@ -1,9 +1,26 @@
-# For Loop
-
+# Loop through a number
 for x in range(10):
     print(x)
 
 print("-------------------------------------")
 
+# Loop through a string
 for i in "Python":
     print(i)
+
+print("-------------------------------------")
+
+# Loop through a List
+friends = ['Bryan', 'Sharon', 'Arnold', 'David', 'Jaspher']
+for friend in friends:
+    print("Hi " + friend)
+
+# Calcualte sum and average of a given List
+values = [25, 90, 1, 23, 21, 6, 18, 4]
+sum = 0
+length = 0
+for value in values:
+    sum += value
+    length += 1
+print("Total sum: " + str(sum))
+print("Average: " + str(sum/length))
