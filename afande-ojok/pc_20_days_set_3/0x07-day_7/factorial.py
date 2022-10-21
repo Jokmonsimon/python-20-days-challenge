fc634fa23d1daee4de973896004d0386c887ffa9
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-10-22 00:23:07 +0300

    Factorial

--- afande-ojok/pc_20_days_set_3/0x07-day_7/factorial.py
+++ afande-ojok/pc_20_days_set_3/0x07-day_7/factorial.py
@@ -2,6 +2,10 @@
 # For example, the factorial of four (4!) is equal to 1*2*3*4=24. 
 # Fill in the blanks to make the factorial function return the right number.
 
+import re
+from unittest import result
+
+
 def factorial(n):
     result = 1
     for i in range(result, n+1):
@@ -11,3 +15,18 @@ def factorial(n):
 print(factorial(4))
 print(factorial(5))
 print(factorial(12))
+
+# Fill in the blanks to make the factorial function return the factorial of n. 
+# Then, print the first 10 factorials (from 0 to 9) with the corresponding number. 
+# Remember that the factorial of a number is defined as the product of an integer and all integers before it. 
+# For example, the factorial of five (5!) is equal to 1*2*3*4*5=120. 
+# Also recall that the factorial of zero (0!) is equal to 1.
+
+def new_factorial(n):
+    result = 1
+    for x in range(1, n+1):
+        result = result * x
+    return result
+print("=====================================\n")
+for n in range(0, 10):
+    print(n, new_factorial(n))
