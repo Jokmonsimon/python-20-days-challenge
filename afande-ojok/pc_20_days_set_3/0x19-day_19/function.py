833e207df81a6382b92c6df77ed4984adeabcc33
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-11-02 22:17:13 +0300

    First commit

---
+++ afande-ojok/pc_20_days_set_3/0x19-day_19/function.py
@@ -0,0 +1,10 @@
+def lyrics():
+    print("I'm a luberjack!")
+    print("I sleep all night and work all day!")
+
+def repeat_lyrics():
+    lyrics()
+    lyrics()
+    lyrics()
+    lyrics()
+repeat_lyrics()
