fafcbf554761dda0c303b636124afc408c707760
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-11-07 22:11:48 +0300

    Working with class constructor methods

--- afande-ojok/pc_20_days_set_4/0x04-day_4/class_apple.py
+++ afande-ojok/pc_20_days_set_4/0x04-day_4/class_apple.py
@@ -2,7 +2,8 @@ class Apple:
     def __init__(self, color, flavor):
         self.color = color
         self.flavor = flavor
+    def __str__(self):
+        return "This apple is {} and its flavor is {}".format(self.color, self.flavor)
 
 jonagold = Apple("red", "sweet")
-print(jonagold.color)
-print(jonagold.flavor)
+print(jonagold)
