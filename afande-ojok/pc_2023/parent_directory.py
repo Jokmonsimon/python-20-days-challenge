b87cfafaf43e743510a2f1f025b810f3942ecadd
Ojok Simon Peter <simonpeter.oj@gmail.com>
2023-01-21 22:22:06 +0300

    New update

--- afande-ojok/pc_2023/parent_directory.py
+++ afande-ojok/pc_2023/parent_directory.py
@@ -3,7 +3,7 @@ import os
 def parent_directory():
     # Create a relative path to the parent of the current working directory
     # os.chdir('..')
-    relative_parent = os.path.join(str(os.getcwd), str(os.chdir('..')))
+    relative_parent = os.path.join(str(os.getcwd()), str(os.chdir('..')))
    # relative_parent = str(relative_parent)
 
     # Return the absolute path of the parent directory
