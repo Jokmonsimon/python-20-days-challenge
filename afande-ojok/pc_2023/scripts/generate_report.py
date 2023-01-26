ce9ff659d8ab4738e18d1dd12d4be202ecfdb8b5
Ojok Simon Peter <simonpeter.oj@gmail.com>
2023-01-26 22:34:40 +0300

    importing employees.csv data complete

--- afande-ojok/pc_2023/scripts/generate_report.py
+++ afande-ojok/pc_2023/scripts/generate_report.py
@@ -4,14 +4,15 @@
 
 import csv
 
+
 def read_employees(csv_file_location):
 	csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
 	employee_file = csv.DictReader(open(csv_file_location), dialect = 'empDialect')
 
 	employee_list = []
-	for dat in employee_file:
+	for data in employee_file:
 		employee_list.append(data)
 		return employee_list
 
-	employee_list = read_employees('<file_location')
+	employee_list = read_employees('/home/yearn/Yearn/python-20-days-challenge/afande-ojok/pc_2023/data/employees.csv')
 	print(employee_list)
